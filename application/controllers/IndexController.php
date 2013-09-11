69a167725ef1eb1e4398673051789d791bc95866
Dillon Lee <dillonl@sdcenter.utah.edu>
2013-09-11 17:38:00 -0600

    first commit

---
+++ application/controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+class IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+		$this->_helper->layout->setLayout('layout');
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+
+}
+
